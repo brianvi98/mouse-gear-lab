ce06a5a220c61414729d573843be8a62898ba7ea
Brian Vi <brianvi98@gmail.com>
2026-03-19 20:06:17 -0700

    feat(capture-hook): improve precision with coalescedevents; limit capture to parent component

--- client/src/hooks/usePointerCapture.ts
+++ client/src/hooks/usePointerCapture.ts
@@ -1,44 +1,74 @@
 import { useRef, useEffect } from "react";
 
 export type PointerDataPoint = {
-  x: number,    // absolute pixel position on x-axis
-  y: number,    // absolute pixel position on y-axis
-  dx: number,   // delta X since last event
-  dy: number,   // delta Y since last event
-  t: number,    // ms since recording started
+  x: number;    // absolute pixel position on x-axis
+  y: number;    // absolute pixel position on y-axis
+  dx: number;   // delta X since last event
+  dy: number;   // delta Y since last event
+  t: number;    // ms since recording started
 }
 
-export const usePointerCapture = (isRecording: boolean = false) => {
+// This hook is intended to be consumed by a parent component that serves as a
+// "canvas" for the capture. Within the parent, this hook can start and stop
+// and stores data pointers for all captures during the elapsed duration
+export const usePointerCapture = <T extends HTMLElement>({ 
+  targetRef,
+  isRecording,
+} : {
+  targetRef: React.RefObject<T | null>,
+  isRecording: boolean,
+}) => {
   const data = useRef<PointerDataPoint[]>([]);
   const startTime = useRef<number | null>(null);
+  const prevPos = useRef<{x: number, y: number} | null>(null);
+
+  const reset = () => {
+    data.current = [];
+    startTime.current = null;
+    prevPos.current = null;
+  }
 
   useEffect(() => {
 
     // when recording stops, reset everything
     if (!isRecording) {
-        data.current = [];
-        startTime.current = null;
-        return;
+      reset();
+      return;
     };
 
-    startTime.current = performance.now();
+    if (startTime.current === null) {
+      startTime.current = performance.now();
+    }
 
     const handleMouseMoveEvent = (e: PointerEvent) => {
+      const coalesced = e.getCoalescedEvents?.() ?? [e];
+      coalesced.forEach(event => {     
+        const dx = prevPos.current ? event.clientX - prevPos.current.x : 0;
+        const dy = prevPos.current ? event.clientY - prevPos.current.y : 0;
+
+        prevPos.current = { x: event.clientX, y: event.clientY };
+
         data.current.push({
-            x: e.clientX,
-            y: e.clientY,
-            dx: e.movementX,
-            dy: e.movementY,
-            t: performance.now() - startTime.current!,
+          x: event.clientX,
+          y: event.clientY,
+          dx,
+          dy,
+          t: event.timeStamp - startTime.current!,
         })
+      })
     };
 
-    window.addEventListener("pointermove", handleMouseMoveEvent);
+    const target = targetRef?.current ?? window;
 
-    return () => 
-      window.removeEventListener("pointermove", handleMouseMoveEvent);
+    target.addEventListener(
+      "pointermove", handleMouseMoveEvent as EventListener
+    );
 
-  }, [isRecording])
+    return () => target.removeEventListener(
+        "pointermove", handleMouseMoveEvent as EventListener
+    );
 
-  return data;
+  }, [isRecording, targetRef])
+
+  return { data };
 }
